90229bd9ccfa27a1708f440cd30d283a2abbfaf5
Luis Alvarez <lalvarezguillen@gmail.com>
2023-03-07 10:33:09 +0100

    suggestions

--- todo.go
+++ todo.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 type Todo struct {
 	ID          int64  `json:"id" xml:"id"`
 	Description string `json:"description" xml:"description"`
 	IsDone      bool   `json:"is_done" xml:"isDone"`
+}
+
+func (t Todo) Validate() error {
+	if t.Description == "" {
+		return fmt.Errorf("unexpected empty description")
+	}
+
+	return nil
 }
